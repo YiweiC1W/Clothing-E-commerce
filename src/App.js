c36bd1ecda5400c43015ae6009b699627ad9c5c6
Yiwei Chen <kanyeishere@gmail.com>
2020-02-10 17:32:58 +1100

    user sign up component and use firestore to store user info

--- src/App.js
+++ src/App.js
@@ -20,9 +20,21 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-			createUserProfileDocument(user);
-			console.log(user)
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+
+					console.log(this.state)
+				});
+			}
+			this.setState({ currentUser: userAuth });
 		});
 	}
 
@@ -45,3 +57,7 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
