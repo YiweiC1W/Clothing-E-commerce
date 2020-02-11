66bf1ea57a9cc759466f110b29f03c8367d40ff1
Yiwei Chen <kanyeishere@gmail.com>
2020-02-11 17:56:23 +1100

    Use redux to handle user state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -17,12 +19,20 @@ const Header = ({ currentUser }) => (
 				CONTACT
 			</Link>
 			{currentUser ? (
-				<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
 			) : (
-				<Link className="option" to="/signin">SIGN IN</Link>
+				<Link className="option" to="/signin">
+					SIGN IN
+				</Link>
 			)}
 		</div>
 	</div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+        currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
